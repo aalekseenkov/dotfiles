990eb0255483510796be9afa10d222883726d7cc
aalekseenkov <andrew.alekseenkov@yandex.by>
2025-05-10 11:31:15 +0300

    update install.sh for light themes

--- .github/install.sh
+++ .github/install.sh
@@ -7,54 +7,48 @@ shopt -s expand_aliases
 sudo apt update
 sudo apt install -y git unzip curl
 
-# install JetBrainsMono Nerd Font
-sudo mkdir -p /usr/share/fonts/JetBrainsMono
-curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep browser_download_url | grep 'JetBrainsMono.zip' | cut -d '"' -f 4)
-sudo unzip ./JetBrainsMono.zip -d /usr/share/fonts/JetBrainsMono/ && rm -f ./JetBrainsMono.zip
-
 # install RobotoMono Nerd Font
 sudo mkdir -p /usr/share/fonts/RobotoMono
 curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep browser_download_url | grep 'RobotoMono.zip' | cut -d '"' -f 4)
 sudo unzip ./RobotoMono.zip -d /usr/share/fonts/RobotoMono/ && rm -f ./RobotoMono.zip
 
-# install Hack Nerd Font
-sudo mkdir -p /usr/share/fonts/Hack
-curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep browser_download_url | grep 'Hack.zip' | cut -d '"' -f 4)
-sudo unzip ./Hack.zip -d /usr/share/fonts/Hack/ && rm -f ./Hack.zip
-
-# install FiraCode Nerd Font
-sudo mkdir -p /usr/share/fonts/FiraCode
-curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep browser_download_url | grep 'FiraCode.zip' | cut -d '"' -f 4)
-sudo unzip ./FiraCode.zip -d /usr/share/fonts/FiraCode/ && rm -f ./FiraCode.zip
-
-# install SauceCodePro Nerd Font
-sudo mkdir -p /usr/share/fonts/SourceCodePro
-curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep browser_download_url | grep 'SourceCodePro.zip' | cut -d '"' -f 4)
-sudo unzip ./SourceCodePro.zip -d /usr/share/fonts/SourceCodePro/ && rm -f ./SourceCodePro.zip
+# install JetBrainsMono Nerd Font
+sudo mkdir -p /usr/share/fonts/JetBrainsMono
+curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep browser_download_url | grep 'JetBrainsMono.zip' | cut -d '"' -f 4)
+sudo unzip ./JetBrainsMono.zip -d /usr/share/fonts/JetBrainsMono/ && rm -f ./JetBrainsMono.zip
 
 # set the correct name for the default profile into Linux Mint Gnome Terminal
 id=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$id/ visible-name 'Default'
 
-# install Nord
-wget https://raw.githubusercontent.com/nordtheme/gnome-terminal/refs/heads/develop/src/nord.sh
-chmod u+x ~/nord.sh
-bash ~/nord.sh
-rm ~/nord.sh
-
 # install Gogh's colorschemes
 wget https://github.com/Gogh-Co/Gogh/raw/master/apply-colors.sh
+# wget https://github.com/Gogh-Co/Gogh/raw/master/installs/nord.sh
 wget https://github.com/Gogh-Co/Gogh/raw/master/installs/catppuccin-frappe.sh
-wget https://github.com/Gogh-Co/Gogh/raw/master/installs/gogh.sh
+wget https://github.com/Gogh-Co/Gogh/raw/master/installs/novel.sh
+wget https://github.com/Gogh-Co/Gogh/raw/master/installs/paper.sh
+wget https://github.com/Gogh-Co/Gogh/raw/master/installs/papercolor-light.sh
 export TERMINAL="gnome-terminal"
 export GOGH_NONINTERACTIVE=
 export GOGH_USE_NEW_THEME=
 chmod u+x ~/apply-colors.sh
+# bash ~/nord.sh
 bash ~/catppuccin-frappe.sh
-bash ~/gogh.sh
+bash ~/novel.sh
+bash ~/paper.sh
+bash ~/papercolor-light.sh
 rm apply-colors.sh
+# rm ~/nord.sh
 rm ~/catppuccin-frappe.sh
-rm ~/gogh.sh
+rm ~/novel.sh
+rm ~/paper.sh
+rm ~/papercolor-light.sh
+
+# install original Nord colorscheme
+wget https://raw.githubusercontent.com/nordtheme/gnome-terminal/refs/heads/develop/src/nord.sh
+chmod u+x ~/nord.sh
+bash ~/nord.sh
+rm ~/nord.sh
 
 # set the font and its size into all the profiles
 profiles=($(gsettings get org.gnome.Terminal.ProfilesList list | tr -d "[]\',"))
