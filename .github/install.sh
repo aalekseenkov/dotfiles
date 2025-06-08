ffa39c028842302a160b3cb915bf704d9f1d0a40
aalekseenkov <andrew.alekseenkov@yandex.by>
2025-06-08 16:43:00 +0300

    update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -13,9 +13,9 @@ curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases
 sudo unzip ./RobotoMono.zip -d /usr/share/fonts/RobotoMono/ && rm -f ./RobotoMono.zip
 
 # install JetBrainsMono Nerd Font
-sudo mkdir -p /usr/share/fonts/JetBrainsMono
-curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep browser_download_url | grep 'JetBrainsMono.zip' | cut -d '"' -f 4)
-sudo unzip ./JetBrainsMono.zip -d /usr/share/fonts/JetBrainsMono/ && rm -f ./JetBrainsMono.zip
+# sudo mkdir -p /usr/share/fonts/JetBrainsMono
+# curl -fsSLO $(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep browser_download_url | grep 'JetBrainsMono.zip' | cut -d '"' -f 4)
+# sudo unzip ./JetBrainsMono.zip -d /usr/share/fonts/JetBrainsMono/ && rm -f ./JetBrainsMono.zip
 
 # set the correct name for the default profile into Linux Mint Gnome Terminal
 id=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
@@ -25,21 +25,23 @@ gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profi
 wget https://github.com/Gogh-Co/Gogh/raw/master/apply-colors.sh
 wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution.sh
 wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-antique-green.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-blueberry-violet.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-deep-violet.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-hibiscus-purple.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-midnight-orchid.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-mono.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-blueberry-violet.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-deep-violet.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-hibiscus-purple.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-midnight-orchid.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-mono.sh
 wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-nostalgia.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-pine-green-4-blue-tinge.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-pine-green-4-blue-tinge.sh
 wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-pine-green.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-purple-rain.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-twilight-lavender.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-violet-dusk.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-violet-flower.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-violet-indigo.sh
-wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-violet-purple.sh
-wget https://github.com/Gogh-Co/Gogh/raw/master/installs/tomorrow.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-purple-rain.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-twilight-lavender.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-violet-dusk.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-violet-flower.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-violet-indigo.sh
+# wget https://github.com/aalekseenkov/dotfiles/raw/refs/heads/master/.github/avalution-violet-purple.sh
+wget https://github.com/Gogh-Co/Gogh/raw/master/installs/paper.sh
+wget https://github.com/Gogh-Co/Gogh/raw/master/installs/tempus-day.sh
+wget https://github.com/Gogh-Co/Gogh/raw/master/installs/tempus-past.sh
 
 export TERMINAL="gnome-terminal"
 export GOGH_NONINTERACTIVE=
@@ -48,47 +50,45 @@ export GOGH_USE_NEW_THEME=
 chmod u+x ~/apply-colors.sh
 bash ~/avalution.sh
 bash ~/avalution-antique-green.sh
-bash ~/avalution-blueberry-violet.sh
-bash ~/avalution-deep-violet.sh
-bash ~/avalution-hibiscus-purple.sh
-bash ~/avalution-midnight-orchid.sh
-bash ~/avalution-mono.sh
+# bash ~/avalution-blueberry-violet.sh
+# bash ~/avalution-deep-violet.sh
+# bash ~/avalution-hibiscus-purple.sh
+# bash ~/avalution-midnight-orchid.sh
+# bash ~/avalution-mono.sh
 bash ~/avalution-nostalgia.sh
-bash ~/avalution-pine-green-4-blue-tinge.sh
+# bash ~/avalution-pine-green-4-blue-tinge.sh
 bash ~/avalution-pine-green.sh
-bash ~/avalution-purple-rain.sh
-bash ~/avalution-twilight-lavender.sh
-bash ~/avalution-violet-dusk.sh
-bash ~/avalution-violet-flower.sh
-bash ~/avalution-violet-indigo.sh
-bash ~/avalution-violet-purple.sh
-bash ~/avalution-violet-purple.sh
-bash ~/tomorrow.sh
+# bash ~/avalution-purple-rain.sh
+# bash ~/avalution-twilight-lavender.sh
+# bash ~/avalution-violet-dusk.sh
+# bash ~/avalution-violet-flower.sh
+# bash ~/avalution-violet-indigo.sh
+# bash ~/avalution-violet-purple.sh
+# bash ~/avalution-violet-purple.sh
+bash ~/paper.sh
+bash ~/tempus-day.sh
+bash ~/tempus-past.sh
 
 rm apply-colors.sh
 rm ~/avalution.sh
 rm ~/avalution-antique-green.sh
-rm ~/avalution-blueberry-violet.sh
-rm ~/avalution-deep-violet.sh
-rm ~/avalution-hibiscus-purple.sh
-rm ~/avalution-midnight-orchid.sh
-rm ~/avalution-mono.sh
+# rm ~/avalution-blueberry-violet.sh
+# rm ~/avalution-deep-violet.sh
+# rm ~/avalution-hibiscus-purple.sh
+# rm ~/avalution-midnight-orchid.sh
+# rm ~/avalution-mono.sh
 rm ~/avalution-nostalgia.sh
-rm ~/avalution-pine-green-4-blue-tinge.sh
+# rm ~/avalution-pine-green-4-blue-tinge.sh
 rm ~/avalution-pine-green.sh
-rm ~/avalution-purple-rain.sh
-rm ~/avalution-twilight-lavender.sh
-rm ~/avalution-violet-dusk.sh
-rm ~/avalution-violet-flower.sh
-rm ~/avalution-violet-indigo.sh
-rm ~/avalution-violet-purple.sh
-rm ~/tomorrow.sh
-
-# install original Nord colorscheme (to compare in the future)
-# wget https://raw.githubusercontent.com/nordtheme/gnome-terminal/refs/heads/develop/src/nord.sh
-# chmod u+x ~/nord.sh
-# bash ~/nord.sh
-# rm ~/nord.sh
+# rm ~/avalution-purple-rain.sh
+# rm ~/avalution-twilight-lavender.sh
+# rm ~/avalution-violet-dusk.sh
+# rm ~/avalution-violet-flower.sh
+# rm ~/avalution-violet-indigo.sh
+# rm ~/avalution-violet-purple.sh
+rm ~/paper.sh
+rm ~/tempus-day.sh
+rm ~/tempus-past.sh
 
 # set the font and its size into all the profiles
 profiles=($(gsettings get org.gnome.Terminal.ProfilesList list | tr -d "[]\',"))
@@ -105,6 +105,8 @@ sudo apt install -y --quiet fish
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > ~/fish_install
 fish ~/fish_install --noninteractive --yes
 rm ~/fish_install
+omf install agnoster
+omf theme agnoster
 
 # Install zsh + OMZ
 sudo apt install -y zsh
@@ -117,9 +119,9 @@ sudo apt install -y dconf-cli uuid-runtime xclip wget bat fzf
 sudo apt install -y tmux vim neovim mc
 
 # Install Catppuccin Frappe Theme for BAT
-mkdir -p "$(batcat --config-dir)/themes"
-wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
-batcat cache --build
+# mkdir -p "$(batcat --config-dir)/themes"
+# wget -P "$(batcat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme
+# batcat cache --build
 
 # Install the latest verion of EZA
 sudo apt install -y gpg
