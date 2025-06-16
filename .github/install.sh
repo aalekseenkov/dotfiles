63429dc65e00964738b2aeb59488cd4e88fac196
aalekseenkov <andrew.alekseenkov@yandex.by>
2025-06-16 18:00:14 +0300

    fix an apt command in the docker installation

--- .github/install.sh
+++ .github/install.sh
@@ -109,8 +109,8 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
