f0383f708f18007ccc7b516e93a82c3637e233b4
aalekseenkov <andrew.alekseenkov@yandex.by>
2025-06-16 23:01:27 +0300

    update docker installation's steps

--- .github/install.sh
+++ .github/install.sh
@@ -59,6 +59,8 @@ for i in ${!profiles[*]}; do
 done
 
 # Install Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -68,9 +70,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# sudo groupadd docker
+sudo groupadd -f docker
 sudo usermod -aG docker $USER
-sudo newgrp docker
+newgrp docker
 
 # Install oh-my-bash
 export OSH="$HOME/.config/oh-my-bash"
