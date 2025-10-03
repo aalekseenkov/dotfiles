98704138f7e9e5eca47bfc3c0c2f7f9e466ae426
aalekseenkov <andrew.alekseenkov@yandex.by>
2025-10-03 10:51:58 +0300

    Format colorscheme.lua

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,40 +8,39 @@ return {
     config = function()
 
       -- neobones
-      -- vim.g.neobones_lightness = 'dim' -- dim, default, bright
+      -- vim.g.neobones_lightness = "dim" -- dim, default, bright
 
       -- forestbones
-      -- vim.g.forestbones_lightness = 'dim' -- dim, default, bright
-      -- vim.g.forestbones_darkness = 'warm' -- warm, stark
+      -- vim.g.forestbones_lightness = "dim" -- dim, default, bright
+      -- vim.g.forestbones_darkness = "warm" -- warm, stark
 
       -- zenbones
-      -- vim.g.zenbones_lightness = 'dim' -- dim, default, bright
+      -- vim.g.zenbones_lightness = "dim" -- dim, default, bright
 
       -- zenwritten
-      -- vim.g.zenwritten_lightness = 'dim' -- dim, default, bright
+      -- vim.g.zenwritten_lightness = "dim" -- dim, default, bright
 
       -- rosebones
-      -- vim.g.rosebones_lightness = 'dim' -- dim, default, bright
+      -- vim.g.rosebones_lightness = "dim" -- dim, default, bright
 
       -- seoulbones
-      -- vim.g.seoulbones_lightness = 'dim' -- dim, default, bright
+      -- vim.g.seoulbones_lightness = "dim" -- dim, default, bright
 
       -- tokyobones
-      -- vim.g.tokyobones_lightness = 'dim' -- dim, default, bright
+      -- vim.g.tokyobones_lightness = "dim" -- dim, default, bright
 
       -- vimbones
-      -- vim.g.vimbones_lightness = 'dim' -- dim, default, bright
+      -- vim.g.vimbones_lightness = "dim" -- dim, default, bright
 
       -- nordbones
-      -- vim.g.nordbones_darkness = 'warm' -- warm, stark
+      -- vim.g.nordbones_darkness = "warm" -- warm, stark
 
       -- zenburned
-      -- vim.g.zenburned_darkness = 'warm' -- warm, stark
+      -- vim.g.zenburned_darkness = "warm" -- warm, stark
 
       -- vim.cmd("set background=light")
       -- vim.cmd("colorscheme zenbones")
-
-    end
+    end,
   },
   -- https://github.com/EdenEast/nightfox.nvim
   {
@@ -49,16 +48,16 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme('nightfox')
-      vim.cmd.colorscheme('dayfox')
-      -- vim.cmd.colorscheme('dawnfox')
-      -- vim.cmd.colorscheme('duskfox')
-      -- vim.cmd.colorscheme('nordfox')
-      -- vim.cmd.colorscheme('terafox')
-      -- vim.cmd.colorscheme('carbonfox')
-   end
+      -- vim.cmd.colorscheme("nightfox")
+      vim.cmd.colorscheme("dayfox")
+      -- vim.cmd.colorscheme("dawnfox")
+      -- vim.cmd.colorscheme("duskfox")
+      -- vim.cmd.colorscheme("nordfox")
+      -- vim.cmd.colorscheme("terafox")
+      -- vim.cmd.colorscheme("carbonfox")
+    end,
   },
--- https://github.com/savq/melange-nvim
+  -- https://github.com/savq/melange-nvim
   {
     "savq/melange-nvim",
   },
