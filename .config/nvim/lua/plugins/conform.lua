6f29354efcfc07fbf5536734b7f5210ac0b84fc0
aalekseenkov <andrew.alekseenkov@yandex.by>
2025-10-11 00:03:53 +0300

    Update conform.lua for yamlfix options

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -6,6 +6,14 @@ return {
         html = { "prettier" },
         yaml = { "yamlfix" },
       },
+      formatters = {
+        yamlfix = {
+          env = {
+            YAMLFIX_WHITELINES = "1",
+            YAMLFIX_SECTION_WHITELINES = "1",
+          },
+        },
+      },
     },
   },
 }
